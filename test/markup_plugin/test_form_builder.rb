3a0e101593861b70b465eb4547d33a223779122a
Nathan Esquenazi <nesquena@gmail.com>
2009-10-26 20:05:27 -0700

    Added context stubs for each test needed for form helpers / builders

--- test/markup_plugin/test_form_builder.rb
+++ test/markup_plugin/test_form_builder.rb
@@ -6,5 +6,59 @@ class TestFormBuilder < Test::Unit::TestCase
     MarkupDemo.tap { |app| app.set :environment, :test }
   end
 
-  # TODO Test the form builder methods
+  context 'for #form_for method' do
+    
+  end
+  
+  # ===========================
+  # AbstractFormBuilder
+  # ===========================
+  
+  context 'for #error_messages method' do
+    
+  end
+  
+  context 'for #text_field method' do
+    
+  end
+  
+  context 'for #text_area method' do
+    
+  end
+  
+  context 'for #password_field method' do
+    
+  end
+  
+  context 'for #file_field method' do
+    
+  end
+  
+  context 'for #submit method' do
+    
+  end
+  
+  # ===========================
+  # StandardFormBuilder
+  # ===========================
+  
+  context 'for #text_field_block method' do
+  
+  end
+  
+  context 'for #text_area_block method' do
+    
+  end
+  
+  context 'for #password_field_block method' do
+    
+  end
+  
+  context 'for #file_field_block method' do
+    
+  end
+  
+  context 'for #submit_block method' do
+    
+  end
 end
--- test/markup_plugin/test_form_helpers.rb
+++ test/markup_plugin/test_form_helpers.rb
@@ -6,5 +6,39 @@ class TestFormHelpers < Test::Unit::TestCase
     MarkupDemo.tap { |app| app.set :environment, :test }
   end
 
-  # Test the form helper methods
+  context 'for #form_tag method' do
+
+  end
+
+  context 'for #field_set_tag method' do
+
+  end
+
+  context 'for #error_messages_for method' do
+
+  end
+
+  context 'for #label_tag method' do
+
+  end
+
+  context 'for #text_field_tag method' do
+
+  end
+
+  context 'for #text_area_tag method' do
+
+  end
+
+  context 'for #password_field_tag method' do
+
+  end
+
+  context 'for #file_field_tag method' do
+
+  end
+
+  context 'for #submit_tag method' do
+
+  end
 end
