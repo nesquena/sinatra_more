710359037dbcca63aeaee6fa494dbe1eed147a3c
Nathan Esquenazi <nesquena@gmail.com>
2009-10-29 21:18:18 -0700

    Refactor warden plugin strategy to be defined as class instead of block

--- lib/sinatra_more/warden_plugin.rb
+++ lib/sinatra_more/warden_plugin.rb
@@ -1,11 +1,32 @@
 require File.dirname(__FILE__) + '/support_lite'
+require 'warden' unless defined?(Warden)
 load File.dirname(__FILE__) + '/markup_plugin/output_helpers.rb'
 Dir[File.dirname(__FILE__) + '/warden_plugin/**/*.rb'].each {|file| load file }
 
 module SinatraMore
   module WardenPlugin
+    # This is the basic password strategy for authentication
+    class BasicPassword < Warden::Strategies::Base
+      def valid?
+        username || password
+      end
+
+      def authenticate!
+        u = User.authenticate(username, password)
+        u.nil? ? fail!("Could not log in") : success!(u)
+      end
+
+      def username
+        params['username'] || params['nickname'] || params['login'] || params['email']
+      end
+
+      def password
+        params['password'] || params['pass']
+      end
+    end
+
     def self.registered(app)
-      raise "WardenPlugin::Error - Install with 'sudo gem install warden' or require 'warden' in your app." unless Warden::Manager
+      raise "WardenPlugin::Error - Install warden with 'sudo gem install warden' to use plugin!" unless Warden && Warden::Manager
       app.use Warden::Manager do |manager|
         manager.default_strategies :password
         manager.failure_app = app
@@ -16,25 +37,7 @@ module SinatraMore
       # TODO Improve serializing methods
       Warden::Manager.serialize_into_session{ |user| user.nil? ? nil : user.id }
       Warden::Manager.serialize_from_session{ |id|   id.nil? ? nil : User.find(id) }
-
-      Warden::Strategies.add(:password) do
-        def valid?
-          username || password
-        end
-
-        def authenticate!
-          u = User.authenticate(username, password)
-          u.nil? ? fail!("Could not log in") : success!(u)
-        end
-        
-        def username
-          params['username'] || params['nickname'] || params['login'] || params['email']
-        end
-        
-        def password
-          params['password'] || params['pass']
-        end
-      end
+      Warden::Strategies.add(:password, BasicPassword)
     end
   end
 end
