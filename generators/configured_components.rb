5e477d5511cb8278b5f4e69099706db761102af4
Nathan Esquenazi <nesquena@gmail.com>
2009-11-06 04:18:59 -0800

    Remove the need to define component_types twice (autocreate list)

--- generators/configured_components.rb
+++ generators/configured_components.rb
@@ -27,12 +27,14 @@ module SinatraMore
       }
 
       def component_types
-        %w[test mock script renderer orm]
+        @component_types
       end
 
+      # Defines a class option to allow a component to be chosen and add to component type list
       # component_option :test, "Testing framework", :aliases => '-t'
       def component_option(name, description, options = {})
         class_option name, :default => default_for(name), :aliases => options[:aliases]
+        (@component_types ||= []) << name.to_s
       end
 
       def default_for(component)
