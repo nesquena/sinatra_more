d4011a6c68d476e00a4d9ebdbb8730e2d0eb2da5
achiu <achiu@Teumessian.local>
2009-11-06 02:24:56 -0800

    datamapper orm generator

---
+++ lib/sinatra_more/sinatra_generator/orms/datamapper_orm_gen.rb
@@ -0,0 +1,30 @@
+module SinatraMore
+  module DatamapperOrmGen
+    
+    DM = <<-DM
+module DatamapperInitializer
+  require 'dm-core'
+  def self.registered(app)
+    app.configure do
+      DataMapper.setup(:default, ENV['DATABASE_URL'])
+    end
+  end
+end
+DM
+
+  USER = <<-USER
+class User
+  include DataMapper::Resource
+  
+  property :name,     String
+  property :username, String
+  property :email,    String
+end
+  USER
+    
+    def build_orm
+      create_file(@root_path + "/config/initializers/datamapper.rb",DM)
+      create_file(@root_path + "/app/models/user.rb",USER)
+    end
+  end
+end
