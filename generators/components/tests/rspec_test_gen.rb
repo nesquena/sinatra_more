83e42d03e31bb83cb0244946e1e05a13dadef820
Nathan Esquenazi <nesquena@gmail.com>
2009-11-06 22:25:05 -0800

    Added support for riot, shoulda, rspec, testspec in test generator

--- generators/components/tests/rspec_test_gen.rb
+++ generators/components/tests/rspec_test_gen.rb
@@ -1,21 +1,19 @@
 module SinatraMore
-  module BaconTestGen
-    TEST = <<-BACON
-\nclass Bacon::Context
-  include Rack::Test::Methods
+  module RspecTestGen
+    TEST = <<-RSPEC
+\nSpec::Runner.configure do |conf|
+  conf.include Rack::Test::Methods
 end
 
 def app
   CLASS_NAME
 end
-BACON
+RSPEC
     def setup_test
       test_config_path = root_path("/test/test_config.rb")
-      inject_into_file(test_config_path, "require 'bacon'\n", :after => "require 'rack/test'\n")
+      inject_into_file(test_config_path, "require 'spec'\n", :after => "require 'rack/test'\n")
       inject_into_file(test_config_path, TEST.gsub(/CLASS_NAME/, @class_name), :after => "set :environment, :test\n")
     end
     
   end
 end
-
-
