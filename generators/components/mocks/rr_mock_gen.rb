ea4e931b52abffc866a89b5b830d4d95bfffacc2
achiu <achiu@Teumessian.local>
2009-11-07 09:35:57 -0800

    added mocha gen

--- generators/components/mocks/rr_mock_gen.rb
+++ generators/components/mocks/rr_mock_gen.rb
@@ -4,8 +4,7 @@ module SinatraMore
     def setup_mock
       inject_into_file(root_path("/test/test_config.rb"), "require 'rr'\n", 
         :after => "require File.dirname(File.dirname(__FILE__)) + \"/app\"\n")
-      inject_into_file(root_path("/test/test_config.rb"), "  include RR::Adapters::RRMethods\n", 
-        :after => /class(.*)\n/)
+      gsub_file root_path("/test/test_config.rb"), /class.*/, "\\1  include RR::Adapters::RRMethods\n"
     end
   end
 end
