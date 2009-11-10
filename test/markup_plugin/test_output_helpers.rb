c25c3265eef1d81aab074045e0b607925f2437d9
Nathan Esquenazi <nesquena@gmail.com>
2009-11-09 23:19:35 -0800

    Added support for content_for / yield_content support in outputhelper

--- test/markup_plugin/test_output_helpers.rb
+++ test/markup_plugin/test_output_helpers.rb
@@ -6,6 +6,20 @@ class TestOutputHelpers < Test::Unit::TestCase
     MarkupDemo.tap { |app| app.set :environment, :test }
   end
 
+  context 'for #content_for method' do
+    should 'work for erb templates' do
+      visit '/erb/content_for'
+      assert_have_selector '.demo h1', :content => "This is content yielded from a content_for"
+      assert_have_selector '.demo2 h1', :content => "This is content yielded with name Johnny Smith"
+    end
+
+    should "work for haml templates" do
+      visit '/haml/content_for'
+      assert_have_selector '.demo h1', :content => "This is content yielded from a content_for"
+      assert_have_selector '.demo2 h1', :content => "This is content yielded with name Johnny Smith"
+    end
+  end
+
   context 'for #capture_html method' do
     should "work for erb templates" do
       visit '/erb/capture_concat'
