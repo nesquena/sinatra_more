d8fa7c3e5ba825b91854c650ff7cd75fd62753de
achiu <achiu@118-231-134-131.adsl.fetnet.net>
2009-11-11 23:15:29 -0800

    fixed user model to include validate and before_save to actually run encrypt_password

--- generators/components/orms/sequel_orm_gen.rb
+++ generators/components/orms/sequel_orm_gen.rb
@@ -13,31 +13,43 @@ end
 SEQUEL
 
     USER = <<-USER
-class User < Sequel::Model(:users)
-  unless table_exists?
-    set_schema do
-      primary_key :id
-      string :name
-      string :username
-      string :email
-      string :crypted_password
+    class User < Sequel::Model(:users)
+      unless table_exists?
+        set_schema do
+          primary_key :id
+          string :name
+          string :username
+          string :email
+          string :crypted_password
+        end
+        create_table
+      end
+
+      attr_accessor :password, :password_confirmation
+
+      def self.authenticate(username, password)
+        user = User.filter(:username => username).first
+        user && user.has_password?(password) ? user : nil
+      end
+
+      def encrypt_password
+        return if password.blank?
+        self.crypted_password = BCrypt::Password.create(password)
+      end
+
+      def has_password?(password)
+        BCrypt::Password.new(crypted_password) == password
+      end
+
+      def before_save
+        encrypt_password
+      end
+
+      def validate
+        errors.add :password, "must not be empty" if crypted_password.blank? && password.blank?
+        errors.add :password, "must match password confirmation" unless password == password_confirmation
+      end
     end
-    create_table
-  end
-  
-  def self.authenticate(username, password)
-    user = User.filter(:username => username).first
-    user && user.has_password?(password) ? user : nil
-  end
-  
-  def encrypt_password
-    self.crypted_password = BCrypt::Password.create(password)
-  end
-  
-  def has_password?(password)
-    BCrypt::Password.new(crypted_password) == password
-  end
-end
 USER
 
     def setup_orm
