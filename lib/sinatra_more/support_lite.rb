a09d64270b6663fc4d3a1622c4927926cdaea83b
Nathan Esquenazi <nesquena@gmail.com>
2009-11-30 16:00:51 -0800

    Require activesupport deprecation module, and fix blank support loading

--- lib/sinatra_more/support_lite.rb
+++ lib/sinatra_more/support_lite.rb
@@ -1,9 +1,19 @@
 # This is for adding specific methods that are required by sinatra_more if activesupport isn't required
 
 unless String.method_defined?(:titleize) && Hash.method_defined?(:slice)
+  require 'active_support/core_ext/kernel'
+  require 'active_support/core_ext/module'
+  require 'active_support/core_ext/class'
+  require 'active_support/deprecation'
   require 'active_support/inflector'
-  require 'active_support/core_ext/blank'
-  require 'active_support/core_ext/class/attribute_accessors'
   require 'active_support/core_ext/hash'
   require 'active_support/core_ext/array'
+end
+
+unless String.method_defined?(:blank?)
+  begin 
+    require 'active_support/core_ext/object/blank'
+  rescue LoadError
+    require 'active_support/core_ext/blank'
+  end
 end
