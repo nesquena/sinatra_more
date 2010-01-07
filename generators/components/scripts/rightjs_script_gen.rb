74435cd63097a5e7f0846bdfd946c59c03ed4d89
selman <selman.ulug@gmail.com>
2010-01-07 21:50:15 +0800

    copy-paste error :)

--- generators/components/scripts/rightjs_script_gen.rb
+++ generators/components/scripts/rightjs_script_gen.rb
@@ -3,7 +3,7 @@ module SinatraMore
 
     def setup_script
       get("http://rightjs.org/builds/current/right-min.js", "public/javascripts/right-min.js")
-      get("http://rightjs.org/builds/current/right-olds-min.js", "public/javascripts/right-min.js")
+      get("http://rightjs.org/builds/current/right-olds-min.js", "public/javascripts/right-olds-min.js")
     end
     
   end
