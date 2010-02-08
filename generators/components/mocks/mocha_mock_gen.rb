26d78bcf86ae3178ed8889fc218a2351a9340af6
Bill Turner <billturner@gmail.com>
2010-02-08 10:51:07 -0500

    Updates for bundler 0.9.2+

--- generators/components/mocks/mocha_mock_gen.rb
+++ generators/components/mocks/mocha_mock_gen.rb
@@ -1,7 +1,7 @@
 module SinatraMore
   module MochaMockGen
     def setup_mock
-      require_dependencies 'mocha', :env => :testing
+      require_dependencies 'mocha', :group => :testing
       insert_mocking_include "Mocha::API", :path => "test/test_config.rb"
     end
   end
