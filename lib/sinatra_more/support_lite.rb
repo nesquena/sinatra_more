6f3d4216e805e49e495e3e0ea7b6c0c4a6ad6cf4
Nathan Esquenazi <nesquena@gmail.com>
2010-02-10 12:03:07 -0800

    move core_ext above deprecation in support_lite

--- lib/sinatra_more/support_lite.rb
+++ lib/sinatra_more/support_lite.rb
@@ -4,10 +4,10 @@ unless String.method_defined?(:titleize) && Hash.method_defined?(:slice)
   require 'active_support/core_ext/kernel'
   require 'active_support/core_ext/array'
   require 'active_support/core_ext/hash'
-  require 'active_support/deprecation'
-  require 'active_support/inflector'
   require 'active_support/core_ext/module'
   require 'active_support/core_ext/class'
+  require 'active_support/deprecation'
+  require 'active_support/inflector'
 end
 
 unless String.method_defined?(:blank?)
