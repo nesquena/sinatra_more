63e8dd936717996ea3117a13fcfaf7006cebb9dc
Nathan Esquenazi <nesquena@gmail.com>
2010-02-10 11:18:54 -0800

    Reorder the support_lite dependencies

--- lib/sinatra_more/support_lite.rb
+++ lib/sinatra_more/support_lite.rb
@@ -1,13 +1,13 @@
 # This is for adding specific methods that are required by sinatra_more if activesupport isn't required
 
 unless String.method_defined?(:titleize) && Hash.method_defined?(:slice)
+  require 'active_support/core_ext/array'
+  require 'active_support/core_ext/hash'
+  require 'active_support/deprecation'
+  require 'active_support/inflector'
   require 'active_support/core_ext/kernel'
   require 'active_support/core_ext/module'
   require 'active_support/core_ext/class'
-  require 'active_support/deprecation'
-  require 'active_support/inflector'
-  require 'active_support/core_ext/hash'
-  require 'active_support/core_ext/array'
 end
 
 unless String.method_defined?(:blank?)
